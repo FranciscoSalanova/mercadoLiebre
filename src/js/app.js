0a51a54cacfdb8f9995e541fedff19f2ab7cc27d
Francisco Salanova <fsalanova.16@gmail.com>
2022-01-24 20:15:54 -0300

    Ajuste de archivo app para reorganizar repo con carpeta js

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, '../../public');
 app.use(express.static(publicPath));
 
 app.listen(process.env.PORT || 3000, () => {
@@ -11,5 +11,5 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, '../views/home.html'));
 });
